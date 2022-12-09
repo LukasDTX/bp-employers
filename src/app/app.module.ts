75125bddbc252abdfd12b45c1f645aff98e4d634
lukas <lukaszhacas@gmail.com>
2022-12-09 12:31:19 +0100

    add material & firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    // AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'bp-employers',
+    appId: '1:1030708908180:web:3d05500ebb321b17ece481',
+    storageBucket: 'bp-employers.appspot.com',
+    apiKey: 'AIzaSyCsLFjIez7j1vvhuzjWsEzP6h9_4PgLgP8',
+    authDomain: 'bp-employers.firebaseapp.com',
+    messagingSenderId: '1030708908180',
+    measurementId: 'G-Y65Y7SSYBL',
+  },
   production: false
 };
 
